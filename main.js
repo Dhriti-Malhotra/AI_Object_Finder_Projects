d312a8eb2b589b80c8ec50210a0682a22001781c
Dhriti-Malhotra <134493853+Dhriti-Malhotra@users.noreply.github.com>
2024-05-01 18:04:20 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 status = "";
+object = [];
+speechSynthetisUtterance = "";
+speechSynthesisAPI = "";
 
 
 function setup()
@@ -22,4 +25,37 @@ function modelLoaded()
 function draw()
 {
     image(video, 0, 0, 480, 380)
+
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult);
+    for(i = 0; i < objects.length; i++)
+    {
+        document.getElementById("status").innerHTML = "Detecting Object";
+        document.getElementById("object_found").innerHTML = "Object Found";
+
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+    }
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+    if(objects[i].label == object_name)
+    {
+        document.getElementById("status").innerHTML = object_name + "Found";
+        document.getElementById("speechSynthesisUtterance").innerHTML = "Found";
+    }
+    else
+    {
+        document.getElementById("object").innerHTML = "Object Not Found";
+    }
 }
